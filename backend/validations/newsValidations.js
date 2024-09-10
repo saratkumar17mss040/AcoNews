8cd1732e2e36d6f7b22950771906cb090ae88b31
saratkumar17mss040 <sarathdon9245@gmail.com>
2024-09-10 15:49:53 +0530

    fix: validation

--- backend/validations/newsValidations.js
+++ backend/validations/newsValidations.js
@@ -2,19 +2,21 @@ import { query } from "express-validator";
 
 export const searchValidationRules = [
   query("q")
-    .isRequired()
+    .notEmpty()
     .withMessage("Query parameter is required")
     .trim()
     .isString()
     .withMessage("Query parameter must be a string")
     .isLength({ min: 1 })
     .withMessage("Query parameter must be at least 1 character long"),
+
   query("lang")
     .optional()
     .isString()
     .withMessage("Language code must be a string")
     .isLength({ min: 2, max: 2 })
     .withMessage("Language code must be exactly 2 characters long"),
+    
   query("country")
     .optional()
     .isString()
